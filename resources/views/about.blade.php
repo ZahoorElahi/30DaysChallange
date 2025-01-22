8d383801e215de1da9fde430aaee673a2074f632
Zahoor Elahi <zahoorelahi1221@gmail.com>
2025-01-22 09:36:14 +0500

    day 2 challange

---
+++ resources/views/about.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>About Page</title>
+    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
+</head>
+
+<body>
+    <div class="flex justify-center items-center h-screen">
+        <h1 class="text-4xl font-bold">This is the About Page</h1>
+    </div>
+</body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Contracts\View\View;
 
 Route::get('/', function () {
     return view('welcome');
+});
+Route::get('/about', function (): View {
+    return view('about');
 });
