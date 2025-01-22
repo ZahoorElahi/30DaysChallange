15d0813f859327fe21eec74e9129022df7f28b99
Zahoor Elahi <zahoorelahi1221@gmail.com>
2025-01-22 10:29:14 +0500

    3rd day Assignment

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -9,11 +9,9 @@
 </head>
 
 <body class="font-sans antialiased dark:bg-black dark:text-white/50">
-    <nav>
-        <a href="/">Home</a>
-        <a href="/about">About</a>
-        <a href="/contact">Contact</a>
-    </nav>
+    <x-nav-link>
+
+    </x-nav-link>
     {{ $slot }}
 </body>
 
